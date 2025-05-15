f5130dd0b4f71dd4d6e06a9255f8a775ff7f40d6
FadeIn64 <danik6052@gmail.com>
2025-05-15 14:29:09 +0400

    add change drivers intervals

--- analyzer/internal/repositories/repository.go
+++ analyzer/internal/repositories/repository.go
@@ -9,6 +9,7 @@ import (
 	"github.com/avito-tech/go-transaction-manager/trm"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"math"
 	"time"
 )
 
@@ -23,6 +24,8 @@ func NewLapRepository(db *pgxpool.Pool, manager trm.Manager) *LapRepository {
 		manager: manager,
 	}
 }
+
+const MaxLapsToOvertake = 11
 
 func (r *LapRepository) ProcessLap(ctx context.Context, lap models.Lap) (*models.LapAnalysis, error) {
 	var analysis models.LapAnalysis
@@ -124,6 +127,77 @@ func (r *LapRepository) ProcessLap(ctx context.Context, lap models.Lap) (*models
 			analysis.CurrentLapTime,
 			analysis.AverageSegmentPace,
 		)
+
+		// Определяем интервалы от следующего гонщика
+		curDriver, err := q.GetDriverStats(ctx, db.GetDriverStatsParams{
+			DriverNumber: lap.DriverNumber,
+			MeetingKey:   lap.MeetingKey,
+			SessionKey:   lap.SessionKey,
+		})
+		if err != nil {
+			return fmt.Errorf("get cur driver stats: %w", err)
+		}
+
+		// Если гонщик едет первым, то считать отставние не откого
+		if curDriver.Position == 1 {
+			return nil
+		}
+
+		nextDriver, err := q.GetDriverByPosition(ctx, db.GetDriverByPositionParams{
+			MeetingKey: lap.MeetingKey,
+			SessionKey: lap.SessionKey,
+			Position:   curDriver.Position - 1,
+		})
+
+		if err != nil {
+			return fmt.Errorf("get next driver: %w", err)
+		}
+
+		nextDriverLap, err := q.GetLap(ctx, db.GetLapParams{
+			DriverNumber:     nextDriver.DriverNumber,
+			LapNumber:        lap.LapNumber,
+			MeetingKey:       lap.MeetingKey,
+			SessionKey:       lap.SessionKey,
+			CompletedSectors: int32(completedSectors),
+		})
+		if err != nil {
+			return fmt.Errorf("get next driver lap: %w", err)
+		}
+
+		timeInterval := nextDriverLap.DateEnd.Time.Sub(dateEnd)
+		interval := float64(timeInterval) / float64(time.Second)
+
+		predictOvertake := MaxLapsToOvertake
+		nextDriverSegmentPace, err := q.GetCurrentSegmentPace(ctx, db.GetCurrentSegmentPaceParams{
+			DriverNumber: nextDriver.DriverNumber,
+			MeetingKey:   lap.MeetingKey,
+			SessionKey:   lap.SessionKey,
+			LapNumber:    lap.LapNumber,
+		})
+		if err != nil {
+			return fmt.Errorf("get next driver segment pace: %w", err)
+		}
+
+		if analysis.AverageSegmentPace != 0 && nextDriverSegmentPace.AveragePace != 0 {
+			paceDiff := analysis.AverageSegmentPace - nextDriverSegmentPace.AveragePace
+			if paceDiff > 0 {
+				predictOvertakeFloat := math.Ceil(interval / paceDiff)
+				if predictOvertakeFloat < 11 {
+					predictOvertake = int(predictOvertakeFloat)
+				}
+			}
+		}
+
+		err = q.UpsertDriversInterval(ctx, db.UpsertDriversIntervalParams{
+			MeetingKey:               lap.MeetingKey,
+			SessionKey:               lap.SessionKey,
+			DriverNumber:             lap.DriverNumber,
+			Interval:                 interval,
+			PredictionLapsToOvertake: pgtype.Int4{Int32: int32(predictOvertake), Valid: true},
+		})
+		if err != nil {
+			return fmt.Errorf("upsert driver interval: %w", err)
+		}
 
 		return nil
 	})
